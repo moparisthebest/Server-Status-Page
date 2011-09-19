70dbeb2608677e301fb629f73696a97ea8f90089
moparisthebest <admin@moparisthebest.com>
2011-09-19 01:02:36 -0400

    Added README and database dump.

---
+++ database.sql
@@ -0,0 +1,95 @@
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `banned`
+--
+
+CREATE TABLE IF NOT EXISTS `banned` (
+  `id` int(11) unsigned NOT NULL,
+  `uid` mediumint(8) unsigned NOT NULL,
+  `uname` varchar(80) NOT NULL,
+  `online` tinyint(1) unsigned NOT NULL default '1',
+  `name` tinytext NOT NULL,
+  `pic_url` tinytext NOT NULL,
+  `ip` varchar(30) NOT NULL,
+  `port` smallint(5) unsigned NOT NULL,
+  `version` smallint(3) unsigned NOT NULL,
+  `time` int(10) unsigned NOT NULL,
+  `info` text NOT NULL,
+  `oncount` int(11) unsigned NOT NULL default '1',
+  `totalcount` int(11) unsigned NOT NULL default '1',
+  `uptime` tinyint(3) unsigned NOT NULL default '100',
+  `ipaddress` varchar(15) NOT NULL,
+  `sponsored` smallint(5) unsigned NOT NULL default '0',
+  `rs_name` tinytext NOT NULL,
+  `rs_pass` tinytext NOT NULL,
+  `vote` int(11) NOT NULL default '0',
+  PRIMARY KEY  (`id`),
+  KEY `uid` (`uid`),
+  KEY `online` (`online`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `servers`
+--
+
+CREATE TABLE IF NOT EXISTS `servers` (
+  `id` int(11) unsigned NOT NULL auto_increment,
+  `uid` mediumint(8) unsigned NOT NULL,
+  `uname` varchar(80) NOT NULL,
+  `online` tinyint(1) unsigned NOT NULL default '1',
+  `name` tinytext NOT NULL,
+  `pic_url` tinytext NOT NULL,
+  `ip` varchar(30) NOT NULL,
+  `port` smallint(5) unsigned NOT NULL,
+  `version` smallint(3) unsigned NOT NULL,
+  `time` int(10) unsigned NOT NULL,
+  `info` text NOT NULL,
+  `oncount` int(11) unsigned NOT NULL default '1',
+  `totalcount` int(11) unsigned NOT NULL default '1',
+  `uptime` tinyint(3) unsigned NOT NULL default '100',
+  `ipaddress` varchar(15) NOT NULL,
+  `sponsored` smallint(5) unsigned NOT NULL default '0',
+  `rs_name` tinytext NOT NULL,
+  `rs_pass` tinytext NOT NULL,
+  `vote` int(11) NOT NULL default '0',
+  PRIMARY KEY  (`id`),
+  KEY `uid` (`uid`),
+  KEY `online` (`online`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `toadd`
+--
+
+CREATE TABLE IF NOT EXISTS `toadd` (
+  `id` int(11) unsigned NOT NULL auto_increment,
+  `uid` mediumint(8) unsigned NOT NULL,
+  `uname` varchar(80) NOT NULL,
+  `online` tinyint(1) unsigned NOT NULL default '1',
+  `name` tinytext NOT NULL,
+  `pic_url` tinytext NOT NULL,
+  `ip` varchar(30) NOT NULL,
+  `port` smallint(5) unsigned NOT NULL,
+  `version` smallint(3) unsigned NOT NULL,
+  `time` int(10) unsigned NOT NULL,
+  `info` text NOT NULL,
+  `oncount` int(11) unsigned NOT NULL default '1',
+  `totalcount` int(11) unsigned NOT NULL default '1',
+  `uptime` tinyint(3) unsigned NOT NULL default '100',
+  `ipaddress` varchar(15) NOT NULL,
+  `sponsored` smallint(5) unsigned NOT NULL default '0',
+  `rs_name` tinytext NOT NULL,
+  `rs_pass` tinytext NOT NULL,
+  `key` varchar(15) NOT NULL,
+  `verified` tinyint(1) unsigned NOT NULL default '0',
+  PRIMARY KEY  (`id`),
+  KEY `uid` (`uid`),
+  KEY `online` (`online`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
